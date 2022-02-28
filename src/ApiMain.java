1b3b5affe41f2f7ef4da920498e83fb824963c6b
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-03-01 05:05:58 +0900

    ApiMain 이름변경

--- src/ApiMain.java
+++ src/ApiMain.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Api {
+public class ApiMain {
     public static void main(String[] args) {
 
         ArrayList<String> my_arr_list = new ArrayList<String>();
