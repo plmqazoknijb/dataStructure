5b38218783ceaad7d740c14e0b637146978e1d80
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-03-01 04:17:12 +0900

    ArrayList - JAVA API 사용해보기

---
+++ src/Api.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Api {
+    public static void main(String[] args) {
+
+        ArrayList<String> my_arr_list = new ArrayList<String>();
+
+        my_arr_list.add("hello");
+        my_arr_list.add("java");
+        my_arr_list.add("world!");
+
+        Iterator<String> it = my_arr_list.iterator();
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
